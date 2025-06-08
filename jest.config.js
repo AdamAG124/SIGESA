9db741d08b6d890599905887705386f961f2ae77
Jeycob BG <135167720+JeycobBG@users.noreply.github.com>
2025-06-08 15:16:34 -0600

    Tests for proveedor module and yaml file for CI (#65)
    * Prueba unitaria de creacion de proveedor
    
    * test seguridad proveedor
    
    * test rendimiento creacion proveedor
    
    * adicion de resultados
    
    * Add GitHub Actions CI pipeline for SIGESA tests
    
    * Cambios de rama hilo
    
    * cambios en el yaml
    
    * correccion de sintaxis en yaml

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  testEnvironment: 'jsdom', // Simula el DOM para pruebas frontend
+  roots: ['<rootDir>/tests'], // Donde están tus archivos .test.js
+  moduleFileExtensions: ['js'],
+  transform: {},
+  testMatch: ['<rootDir>/tests/**/*.test.js']
+};
