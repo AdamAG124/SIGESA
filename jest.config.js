19cba4d55b4e64ac7ec10391ae17ad94dc4e1ff4
Jeycob Barrientos García <jeycobbarrientosgarcia@gmail.com>
2025-06-08 11:10:42 -0600

    Prueba unitaria de creacion de proveedor

---
+++ jest.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  testEnvironment: 'jsdom', // Simula el DOM para pruebas frontend
+  roots: ['<rootDir>/tests'], // Donde están tus archivos .test.js
+  moduleFileExtensions: ['js'],
+  transform: {},
+};
